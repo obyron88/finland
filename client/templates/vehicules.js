4ec05ea3f732159753a0d729adc9db226faaf88f
obyron88 <coda2017@yahoo.com>
2017-11-15 16:39:01 +0100

    sec

--- client/templates/vehicules.js
+++ client/templates/vehicules.js
@@ -1,13 +1,29 @@
 Meteor.subscribe("vehicules");
 
-Template.vehicules.helpers({
+Template.marque.helpers({
 // _.uniq repère les entrées distinctes dans la collection vehicules
     vehicules: function (){ return _.uniq(Vehicules.find({}, {
         // sort tri par ordre alphabétique avec 1 (-1 tri ordre inversé)/ fields cherche dans la collection les fields Vehicle_Make
-        sort: {Vehicle_Make: 1}, fields: {Vehicle_Make: true}
+        sort: {VehicleMake: 1}, fields: {VehicleMake: true}
     }).map(function(x) {
-        return x.Vehicle_Make;
+        return x.VehicleMake;
     }), true);
     }
 });
 
+Template.modèle.helpers({
+    model: function (){ return _.uniq(Vehicules.find({}, {
+        // sort tri par ordre alphabétique avec 1 (-1 tri ordre inversé)/ fields cherche dans la collection les fields Vehicle_Make
+        sort: {VehicleDescription: 1}, fields: {VehicleDescription: true}
+    }).map(function(x) {
+        return x.VehicleDescription;
+    }), true);
+    }
+});
+// Template.marque.events({
+//     "select #marque"(event) {
+//         event.preventDefault();
+//         var marqueselect =  event.target.marque.value;
+//
+//     }
+// });
